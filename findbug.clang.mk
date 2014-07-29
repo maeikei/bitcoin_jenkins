2d7d9b62c834866dfcdee3b24357aeb8b8b20aaa
Ma Yingkui <maeikei@gmail.com>
2014-07-29 22:57:14 +0900

    now build find Bitcoind only

--- findbug.clang.mk
+++ findbug.clang.mk
@@ -1,13 +1,15 @@
 WC := $(shell pwd)
 
-.PHONY scan-build bitcoin
-scan-build:
-  cd $(WC)/bitcoin && scan-build -o $(WC)/obj-bitcoin make -j4
+.PHONY: scan-build pre-build bitcoin 
+scan-build:pre-build bitcoin
+	cd $(WC)/bitcoin && scan-build -o $(WC)/obj-bitcoin make -j4
 
 pre-build:
 	mkdir -p $(WC)/obj-bitcoin
 	
 bitcoin:
 	cd $(WC)/bitcoin && ./autogen.sh
-	cd $(WC)/bitcoin && ./configure
+	cd $(WC)/bitcoin && ./configure --with-incompatible-bdb \
+	--with-boost-libdir=/usr/lib/arm-linux-gnueabihf\
+	
 	
