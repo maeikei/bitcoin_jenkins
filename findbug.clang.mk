8ed613da6fbb7ec23e2f11299995d67ad0ada672
ma yingkui <maeikei@gmail.com>
2014-07-29 22:31:27 +0900

    Update findbug.clang.mk

--- findbug.clang.mk
+++ findbug.clang.mk
@@ -1,3 +1,13 @@
-.PHONY clang
-clang:
-  cd bitcoin && scan-build -o ../obj-bitcoin make -j4
+WC := $(shell pwd)
+
+.PHONY scan-build bitcoin
+scan-build:
+  cd $(WC)/bitcoin && scan-build -o $(WC)/obj-bitcoin make -j4
+
+pre-build:
+	mkdir -p $(WC)/obj-bitcoin
+	
+bitcoin:
+	cd $(WC)/bitcoin && ./autogen.sh
+	cd $(WC)/bitcoin && ./configure
+	
