75ab8522b046f3f50eb0bb824158ea77a2a9b565
ma yingkui <maeikei@gmail.com>
2014-07-30 13:33:03 +0900

    Update findbug.clang.mk

--- findbug.clang.mk
+++ findbug.clang.mk
@@ -105,7 +105,7 @@ TARGET      := bitcoin
 
 .PHONY: scan-build pre-build $(TARGET) 
 scan-build:pre-build $(TARGET)
-	cd $(WC)/$(TARGET) && scan-build -o $(REPORTS_DIR) -stats -k $(SCAN_CHECKER) make
+	cd $(WC)/$(TARGET) && scan-build -o $(REPORTS_DIR) -k $(SCAN_CHECKER) make
 
 pre-build:
 	mkdir -p $(REPORTS_DIR)
